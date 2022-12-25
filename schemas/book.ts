0edcacbf56bcb8a16d4f6e7c90102fc7ea302ac3
indegser <indegser@gmail.com>
2022-12-25 12:29:06 +0900

    feat: Add eslint with prettier

--- schemas/book.ts
+++ schemas/book.ts
@@ -1,19 +1,19 @@
-import { SchemaTypeDefinition } from "sanity";
+import {SchemaTypeDefinition} from 'sanity'
 
 export const book: SchemaTypeDefinition = {
-    title: "책",
-    name: "book",
-    type: "document",
-    fields: [
-      {
-        title: "제목",
-        name: "title",
-        type: "string"
-      },
-      {
-        title: '포스터',
-        name: 'poster',
-        type: 'image'
-      }
-    ]
-  } 
+  title: '책',
+  name: 'book',
+  type: 'document',
+  fields: [
+    {
+      title: '제목',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      title: '포스터',
+      name: 'poster',
+      type: 'image',
+    },
+  ],
+}
