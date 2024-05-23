106c385d16251f1ea67d2fb87cf58771a6dc90ce
indegser <indegser@gmail.com>
2024-05-23 20:31:15 +0900

    feat: Add sanity studio

---
+++ src/libs/sanity/client.ts
@@ -0,0 +1,12 @@
+import {createClient} from 'next-sanity'
+
+const projectId = 'em8nd69q'
+const dataset = 'development'
+const apiVersion = '2023-05-03'
+
+export const sanityClient = createClient({
+  projectId,
+  dataset,
+  apiVersion, // https://www.sanity.io/docs/api-versioning
+  useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+})
